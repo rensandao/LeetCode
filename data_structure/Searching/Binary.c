bf28b00148bed3308607345f6e50c3789c4e1036
rensandao <38071371+rensandao@users.noreply.github.com>
2018-08-30 18:11:18 +0800

    Create Binary.c
    折半查找算法

---
+++ data_structure/Searching/Binary.c
@@ -0,0 +1,22 @@
+/* 折半查找，复杂度O(logn)
+具有n个结点的完全二叉树的深度为(log(2)n)+1;
+折半查找最坏情况也是。
+*/
+int Binary_Search(int *a, int n, int key)
+{
+  int low, high, mid;
+  low=1; //最低下标
+  high=n;
+  while(low<=high)
+  {
+    mid=(low+high) /2;
+    if(key<a[mid])
+      high=mid-1;
+    else if (key>a[min])
+      low =mid+1;
+    else
+      return mid;   //mid 即为查找到的位置
+  }
+
+  return 0;
+}

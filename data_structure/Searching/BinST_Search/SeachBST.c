1f9e94c157520a357438d4d586cdaccdfb8ee249
rensandao <38071371+rensandao@users.noreply.github.com>
2018-08-31 01:04:20 +0800

    Create SeachBST.c

---
+++ data_structure/Searching/BinST_Search/SeachBST.c
@@ -0,0 +1,29 @@
+/*二叉树的二叉链表节点结构定义*/
+typedef struct BiTNode
+{
+  int data;     
+  struct BiTNode *lchild, *rchild; // left and right childen pointers.
+}BiTNode, *BiTree;
+
+/*
+递归查找二叉排序树T中是否存在key，
+指针f
+*/
+Status SearchBST(BiTree T, int key, BiTree f, BiTree *p)
+{
+  if(!T)    /*若为空，则不查找*/
+  {
+    *p=f;
+    return FALSE;
+  }
+  else if (key== T->data)
+  {
+    *p = T;
+    return TRUE;
+  }
+  else if (key< T->data)
+    return SearchBST(T->lchild, key, T, p);
+  else
+    return SearchBST(T->rchild, key, T, p);
+  
+}

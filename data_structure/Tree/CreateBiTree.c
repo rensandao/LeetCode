e11d516691f127fc085b553eb9311ddb994eace8
rensandao <38071371+rensandao@users.noreply.github.com>
2018-08-31 17:03:05 +0800

    Create CreateBiTree.c

---
+++ data_structure/Tree/CreateBiTree.c
@@ -0,0 +1,20 @@
+/*按前序输入结点的值（一个字符）
+#表示空树，构建二叉链表表示二叉树T。
+*/
+
+void CreateBiTree(BiTree *T)
+{
+	TElemType ch;
+	scanf("%c",&ch);
+	if(ch=='#')
+		*T=NULL;
+	else
+	{
+		*T=(BiTree)malloc(sizeof(BiTNode));   // ??
+		if(!*T)
+			exit(OVERFLOW);
+		(*T)->data = ch;
+		CreateBiTree(&(*T)-> lchild); //构造左子树
+		CreateBiTree(&(*T)-> rchild);  //构造右子树
+	}
+}

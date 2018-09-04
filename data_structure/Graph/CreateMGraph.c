afdc3d5386cc5cef6d92f5b003433c95fe3a6d7e
rensandao <38071371+rensandao@users.noreply.github.com>
2018-09-04 21:45:32 +0800

    Update CreateMGraph.c

--- data_structure/Graph/CreateMGraph.c
+++ data_structure/Graph/CreateMGraph.c
@@ -11,3 +11,24 @@ typedef struct
 	Edge arc[MAXVEX][MAXVEX];
 	int numVertexes, numEdges;
 }MGraph;
+
+/*建立无向网图的邻接矩阵表示*/
+void CreateMGraph(MGaph *G)   //?
+{
+	int i,j,k,w;
+	print("请输入顶点数和边数:\n ");
+	scanf("%d,%d",&G->numVertexes,&G->numEdges); //输入顶点数和边数；
+	for(i=0; i<G->numVertexes;i++)
+		scanf(&G->vexs[i]);
+	for(i=0;i<G->numVertexes;i++)
+	   for(j=0;j<G->numVertexes;j++)
+		   G->arc[i][j]=INFINITY;
+	for(k=0;k<G->numEdges;k++)
+	{
+		print("输入边（vi,vj）上的下标i，下标j和权重w：\n");
+		scanf("%d,%d,%d",&i,&j,&w);
+		G->arc[i][j]=w;
+		G->arc[i][j] = G->arc[i][j];
+	}
+
+}

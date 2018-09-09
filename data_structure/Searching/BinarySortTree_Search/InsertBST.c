9c443418583559f7c73a49035f54245a20dd46c1
rensandao <38071371+rensandao@users.noreply.github.com>
2018-09-09 16:16:15 +0800

    Create InsertBST.c

---
+++ data_structure/Searching/BinarySortTree_Search/InsertBST.c
@@ -0,0 +1,24 @@
+/* 二叉排序树的插入操作 */
+/* 
+前提：二叉排序树T中不存在关键字等于key的数据元素时，插入key并返回TRUE,否则返回FALSE.
+*/
+
+Status InsertBST(BiTree *T, int key)
+{
+  BiTree p,s;
+  if(!SearchBST(*T,key,NULL,&p)
+  {
+    s = (BiTree)malloc(sizeof(BiTNode));
+    s->data =key;
+    s->lchild = s->rchild = NULL;
+    if(!p)
+      *T=s;
+    else if(key<p->data)
+      p->lchild =s;
+    else
+      p->rchild =s;
+    return TRUE;
+  }
+  else
+    return FALSE; //
+}

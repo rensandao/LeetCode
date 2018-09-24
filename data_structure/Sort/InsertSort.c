517f0844c0a2c373c336e843aa91577a7c779cb6
rensandao <38071371+rensandao@users.noreply.github.com>
2018-09-24 21:12:38 +0800

    Update InsertSort.c

--- data_structure/Sort/InsertSort.c
+++ data_structure/Sort/InsertSort.c
@@ -1,4 +1,3 @@
-//插入排序
 void InsertSort(SqList *L)
 {
   int i,j;
@@ -8,8 +7,9 @@ void InsertSort(SqList *L)
       {
         L->r[0] = L->r[i];  //设置哨兵
         for(j=i-1; L->[j]>L->r[0];j--)
-          L->r[j+1] = L->r[j];
-        L->r[j+1] = L->r[0];
+          L->r[j+1] = L->r[j];   // 记录后移
+        
+        L->r[j+1] = L->r[0]; //插入正确的位置
       
       }
   }

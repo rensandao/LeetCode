b049d2b85ed7c30b31e839ce8a2fdb748e14c057
rensandao <38071371+rensandao@users.noreply.github.com>
2018-11-07 18:15:29 +0800

    typedef

--- data_structure/Tree/Parent_tree.c
+++ data_structure/Tree/Parent_tree.c
@@ -7,7 +7,7 @@ typedef struct PTNode            // 节点结构
   TElemType data;
   int parent;
 } PTNode;
-tydef struct                    //树结构
+typedef struct                    //树结构
 {
   PTNode nodes{MAX_TREE_SIZE};  //结点数组
   int r,n;                     //根位置和结点数

2e41793571eab7c37a232707dacbf85ebddd000f
rensandao <38071371+rensandao@users.noreply.github.com>
2018-11-28 16:43:26 +0800

    Update soultion.cpp

--- 144. Binary Tree Preorder Traversal/soultion.cpp
+++ 144. Binary Tree Preorder Traversal/soultion.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <stack>
-
 using namespace std;
+
+struct TreeNode{
+  int val;
+  TreeNode *left, *right;
+  TreeNode(int x): val(x), left(NULL), right(NULL); //Initialization
+
+};
+
 
 int main(){
 

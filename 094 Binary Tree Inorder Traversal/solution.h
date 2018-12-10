9d288d087b4e8109ef772ca885cd3ff72d0d7f3f
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-10 17:13:02 +0800

    Create solution.h

---
+++ 094 Binary Tree Inorder Traversal/solution.h
@@ -0,0 +1,45 @@
+/****************************************************************
+source: 
+Author: rensandao
+Date: 2018-12-08
+
+Code format and structure reference: 
+https://github.com/pezy/LeetCode/blob/master/093.%20Binary%20Tree%20Inorder%20Traversal/solution.h
+****************************************************************/
+
+#include<vector>
+#include<stack>
+using namespace std; // std::vector, std::stack
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> st;
+        TreeNode *temp = root;
+        
+        while (!st.empty() || temp != nullptr ) {
+            if(temp != nullptr){
+                st.push(temp);
+                temp = temp->left;
+            }
+            else{
+                temp = st.top();
+                res.push_back(temp->val);
+                st.pop();
+                
+                temp = temp->right;            
+            }           
+        }
+             
+        return res;   
+    }
+};

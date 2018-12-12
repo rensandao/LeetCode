3f61a02c9da5d261d397b69f2e28a8232900f318
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-12 23:29:48 +0800

    Update solution.h

--- 145. Binary Tree Postorder Traversal/solution.h
+++ 145. Binary Tree Postorder Traversal/solution.h
@@ -13,6 +13,11 @@ struct TreeNode {
  };
 
 //#1 postorder traversal using stack
+/*
+思路1：
+思维的出发可以对比先序遍历，因为先序和后续遍历顺序中，都是先left或right。那么入栈的顺序也应该都是先压右孩子结点，在左孩子结点。
+
+*/
 class solution {
 public:
    vector<int> postorderTraversal(TreeNode* root){

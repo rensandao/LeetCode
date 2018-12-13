5b4fac41048bd0b4ca58de289882dad4268d00e3
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-14 00:17:21 +0800

    Update solution.h

--- 102. Binary Tree Level Order Traversal/solution.h
+++ 102. Binary Tree Level Order Traversal/solution.h
@@ -1,10 +1,14 @@
-
- Definition for a binary tree node.
- struct TreeNode {
-     int val;
-     TreeNode *left;
-     TreeNode *right;
-     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+/****************************************************************
+source: 
+Author: rensandao
+Date: 2018-12-14
+****************************************************************/ 
+//Definition for a binary tree node.
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  };
  
 class Solution {

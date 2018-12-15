010e1a8fde43987acc2ed8bd9d731bf369df9e7a
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-15 23:27:28 +0800

    Update BinaryTreeLevelOrderTraversalII.cpp

--- 107. Binary Tree Level Order Traversal II/BinaryTreeLevelOrderTraversalII.cpp
+++ 107. Binary Tree Level Order Traversal II/BinaryTreeLevelOrderTraversalII.cpp
@@ -8,7 +8,6 @@ First Creation: 2018-12-15
 
 using std::vector;
 
-
 //Definition for a binary tree node.
 struct TreeNode {
     int val;
@@ -16,16 +15,29 @@ struct TreeNode {
     TreeNode *right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  };
- 
+
+
  /*
  181215
- 思路1：
+ 思路1：如果用继续用队列的方式，行不行
+
+*/
+
+
+
+
+ /*
+ 181215
+ 思路2：利用insert的方法，每一层的vector插入到首段之前。
  
  
  */
 //using
 //method1
 vector<vector<int>> levelOrderBottom1(TreeNode* root) {
-
+	
 
 }
+
+
+

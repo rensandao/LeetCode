2b66ba6e56a7d58d2dfc71244d48bc501ed76cef
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-17 12:05:30 +0800

    Update solution.h

--- 102. Binary Tree Level Order Traversal/solution.h
+++ 102. Binary Tree Level Order Traversal/solution.h
@@ -19,9 +19,9 @@ struct TreeNode {
 考虑到每次循环，vlevel的数据需要清空，还得需要一个备用容器。这还是这是结点指针。对于数据的存储也得考虑个容器，还得备份。这都用多少个容器了。
 直到看到queue，才再一次感受到数据结构的巧妙之处。这应该就是最底层的轮子了。你自己造轮子还是太嫩，得记住它再多用它。
 */
-vector<vector<int>> levelOrder0(TreeNode *root) {
+vector<vector<int> > levelOrder0(TreeNode *root) {
     if(!root)  return{};
-    vector<vector<int>> res;
+    vector<vector<int> > res;
     vector<TreeNode*> vec_ptr;
     vec_ptr.push_back(root);
     
@@ -59,8 +59,8 @@ reference: LeetCode 100% faster。
 */
 //level order traversal using queue
 //mehtod1
-vector<vector<int>> levelOrder1(TreeNode *root) {
-	vector<vector<int>> res;
+vector<vector<int> > levelOrder1(TreeNode *root) {
+	vector<vector<int> > res;
 	queue<TreeNode*> q;
 	q.push(root);
 	
@@ -90,14 +90,14 @@ reference: Leetcode sample 0ms 100%faster
 //level order traversal using recursive iteration 
 //recursive
 //method2
-vector<vector<int>> levelOrder2(TreeNode *root) {
+vector<vector<int> > levelOrder2(TreeNode *root) {
 	if(!root) return{};
 	vector<vector<int>> res;
 	levelOrderRecursive(root, 1, res);
 	return res;
 }
 
-void levelOrderRecursive(TreeNode *temp, int level, vector<vector<int>> &res) {
+void levelOrderRecursive(TreeNode *temp, int level, vector<vector<int> > &res) {
 	//res内的vector个数正好代表了有几层。如果进入下一层，则在res中先初始化一组空vector，以便后续push_back
 	if (res.size() < level)  res.push_back({});  //c++11: result.emplace_back(std::vector<int>());
 	res[level-1].push_back(temp->val);

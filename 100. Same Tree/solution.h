2019c0c86c965a75af5d40d3284f00d7a195df70
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-20 00:30:43 +0800

    Update solution.h

--- 100. Same Tree/solution.h
+++ 100. Same Tree/solution.h
@@ -11,18 +11,33 @@ struct TreeNode {
  
 
 /*
-思路1：
+思路1： 双栈。
+怎么分析？
+数据结构用什么？
 
 */
 //are two trees same?
 //method1. 
-bool isSameTree(TreeNode* p, TreeNode* q) {
-	bool res;
-	
-	
-	
-	
-	
-	return res;
+bool isSameTree1(TreeNode* p, TreeNode* q) {
+	stack<TreeNode*> st1, st2;
+	st1.push(p); st2.push(q);
+
+	while (!st1.empty()) {
+		TreeNode *c1, *c2;
+		c1 = st1.top(); st1.pop();
+		c2 = st2.top(); st2.pop();
+		//这几个条件要周密
+		if (!c1 && !c2)  continue;
+		if (!c1 || !c2)  return false;
+		if(c1->val != c2->val) return false;
+
+		//不能加if(c1->left)
+		st1.push(c1->left);
+		st1.push(c1->right);
+
+		st2.push(c2->left);
+		st2.push(c2->right);
+	}
+	return true;        
 }
 

4a0a3c299014fb678c668f0f43f33b7d77c5f44a
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-20 00:59:12 +0800

    Update SymmetricTree.cpp

--- 101. Symmetric Tree/SymmetricTree.cpp
+++ 101. Symmetric Tree/SymmetricTree.cpp
@@ -1,5 +1,35 @@
 /****************************************************************
 source: https://github.com/rensandao/LeetCode/new/master/101.%20Symmetric%20Tree
 Author: rensandao
-First Creation: 2018-12-08
+First Creation: 2018-12-20
 ****************************************************************/ 
+struct TreeNode{
+	ElemType val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(size_t x): val(x), left(NULL), right(NULL){}	//initialization
+};
+
+/*
+181220
+题干提示：递归和迭代两种方式。
+
+*/
+class Solution {
+public:
+	
+	//思路1：递归。
+    bool isSymmetric1(TreeNode *root) {
+         
+		
+        
+        
+    }
+	
+	//思路2：迭代。
+	bool isSymmetric2(TreeNode *root) {
+	
+	
+	}
+	
+};

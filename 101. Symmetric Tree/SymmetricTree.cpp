ca9c42f35da25a31b41aa442944a45aaef37c5f3
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-20 13:43:17 +0800

    Update SymmetricTree.cpp

--- 101. Symmetric Tree/SymmetricTree.cpp
+++ 101. Symmetric Tree/SymmetricTree.cpp
@@ -18,15 +18,15 @@ struct TreeNode{
 class Solution {
 public:
 	
-	//思路1：递归。
-	/*
-	181220
-	递归的本质是将问题分解成两个或者多个小问题，那么首先便先要找出这个最小问题及其解。再利用递归调用。
+/*
+181220
+思路1：递归。
+递归的本质是将问题分解成两个或者多个小问题，那么首先便先要找出这个最小问题及其解。再利用递归调用。
 首先最小问题是什么？称之为问题核。
 答：在二叉树的结构上找突破口。求对称性：
 1）整体上，左侧与右侧同一层上的结点，左侧的某个结点的左孩子对应右侧对应结点的右孩子。
 2）对应的结点的确定，源头是从根节点root开始，root到第二层root->left和root->right,但是实际第二层并不能满足这个【问题核】的要素，必要要考虑第三层，
-第二层到第三层之间，一个结点点有两个子结点，这才有了普遍性，可利用性。也就是问题核的解的重利用-递归。
+在第二层到第三层之间，一个结点有两个子结点，这才有了普遍性，可利用性。也就是问题核的解的重利用-递归。
 
 判断对不对称所有的可能性。
 	a.root-left.val == root->right.val,
@@ -34,17 +34,32 @@ public:
 	c.left和right如果有一个不为空，则说明不对称，返回false。
 	d.剩下的是有结点数据的情况，判断a条件成不成立，如果不成立，则返回false；如果成立，则返回true，这个时候开始要进入下一层各自的对称结点，
 	考虑使用递归调用了。
-	
-	
-	*/
-    bool isSymmetric1(TreeNode *root) {
-         
-		
-      
+*/
+	//recursive
+    bool isSymmetric(TreeNode *root) {
         
+        if(!root)  return true;
+        if(!root->left && !root->right)  return true;
+        if(!root->left || !root->right)  return false;
+        
+        return isSymmetricHelp(root->left, root->right);            
+    }
+    
+    bool isSymmetricHelp(TreeNode *node1, TreeNode *node2) {
+        
+        if (!node1 && !node2)  return true;
+        if (!node1 || !node2)  return false;
+        return (node1->val == node2->val)  
+                && isSymmetricHelp(node1->left, node2->right)
+                && isSymmetricHelp(node1->right, node2->left);
     }
 	
-	//思路2：迭代。
+/*
+181220
+思路2：迭代。
+
+*/
+	//iterative
 	bool isSymmetric2(TreeNode *root) {
 	
 	

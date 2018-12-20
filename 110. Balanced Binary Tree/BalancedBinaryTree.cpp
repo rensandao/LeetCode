01a09445ff2fd94e8e209b9e5511639c4b17d5c4
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-20 14:55:45 +0800

    Update BalancedBinaryTree.cpp

--- 110. Balanced Binary Tree/BalancedBinaryTree.cpp
+++ 110. Balanced Binary Tree/BalancedBinaryTree.cpp
@@ -13,6 +13,15 @@ struct TreeNode {
 
 class Solution {
 public:
+
+/*
+181220
+思路1：递归。
+平衡二叉树的定义：a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+即任意结点的两个子树的高度差不超过1.
+
+
+*/
     bool isBalanced(TreeNode* root) {
        
        

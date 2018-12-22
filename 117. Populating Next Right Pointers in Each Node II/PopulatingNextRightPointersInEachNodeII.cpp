c832c7797d14e743aa6ff63b449617cbc691ff5e
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-23 00:57:08 +0800

    Update PopulatingNextRightPointersInEachNodeII.cpp

--- 117. Populating Next Right Pointers in Each Node II/PopulatingNextRightPointersInEachNodeII.cpp
+++ 117. Populating Next Right Pointers in Each Node II/PopulatingNextRightPointersInEachNodeII.cpp
@@ -1,7 +1,7 @@
 /**************************************************
 Source:
 Author: rensandao
-First Creation: 2018-12-20
+First Creation: 2018-12-23
 ***************************************************/
 //Definition for binary tree with next pointer.
 struct TreeLinkNode {
@@ -10,9 +10,25 @@ struct TreeLinkNode {
     TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
  };
 
+
+/*
+181223
+思路1：递归
+
+*/
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
         
     }
 };
+
+/*
+181223
+思路2：迭代。层序遍历（BFS）用queue
+*/
+
+/*
+181223
+思路3：迭代。O(1)空间解法。
+*/

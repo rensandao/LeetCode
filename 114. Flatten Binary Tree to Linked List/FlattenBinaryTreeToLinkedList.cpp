c7133897b54bc0a6e74f3188b2c6c1292f42cd96
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-22 23:50:29 +0800

    Update FlattenBinaryTreeToLinkedList.cpp

--- 114. Flatten Binary Tree to Linked List/FlattenBinaryTreeToLinkedList.cpp
+++ 114. Flatten Binary Tree to Linked List/FlattenBinaryTreeToLinkedList.cpp
@@ -3,17 +3,25 @@ Source:
 Author: rensandao
 First Creation: 2018-12-22
 ***************************************************/
+//Definition for a binary tree node.
+struct TreeNode {
+     int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 /*
-Given a binary tree, flatten it to a linked list in-place.
+181222
+思路1：类Morris。
+1）我们可以从所给的例子推理，结点2要变成结点1的右结点，3要接到2后面。这可以通过赋值root->right实现。但是问题是，如果直接改变，那各个root的右结点
+怎么办，不就断了线索了嘛。所以在操作之前得先把这些后面的值要连接到其前面的结点才行。
 
-For example, given the following tree:
-
-    1
+    1                       
    / \
   2   5
  / \   \
 3   4   6
-The flattened tree should look like:
 
 1
  \
@@ -26,21 +34,6 @@ The flattened tree should look like:
         5
          \
           6
-*/
-//Definition for a binary tree node.
-struct TreeNode {
-     int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-/*
-181222
-思路1：类Morris。
-1）
-
-
 */
 class Solution {
 public:

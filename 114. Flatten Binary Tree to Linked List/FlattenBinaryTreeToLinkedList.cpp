dc78891b1e2952dcc7d822f784d0bce070679c6b
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-23 21:22:54 +0800

    Update FlattenBinaryTreeToLinkedList.cpp

--- 114. Flatten Binary Tree to Linked List/FlattenBinaryTreeToLinkedList.cpp
+++ 114. Flatten Binary Tree to Linked List/FlattenBinaryTreeToLinkedList.cpp
@@ -37,7 +37,7 @@ struct TreeNode {
 
 /*
 181222
-思路1：类Morris。
+思路1：迭代，类Morris。
 1）我们可以从所给的例子推理，结点2要变成结点1的右结点，3要接到2后面。这可以通过赋值root->right实现。但是问题是，如果直接改变，那各个root的右结点
 怎么办，不就断了线索了嘛。所以在操作之前得先把这些后面的值要连接到其前面的结点才行。
 2）也就是如下图，结点5要先连接到结点4后，这时候结点1空了，便可以将结点2接到1的右侧；同理，对于结点4和3也是，4接到3后，3再接到2后。如此循环。
@@ -53,15 +53,31 @@ struct TreeNode {
 												                             \                \
 													                          6				   6			
                                                      
-3）这让我想到了morris算法。
-4）不过，需要注意的是步骤2）中的嫁接操作的顺序，也可以有多种方式。这会决定算法的一些变动。
+3）这让我想到了morris算法思想。不过这里写出来会简单些。
+4）需要注意的是步骤2）中的嫁接操作的顺序，也可以有多种方式。这会导致算法的一些变动。
+
+时间复杂度：
+空间复杂度：
+Runtime: 8ms, 53.80% faster.
 */
+//
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        
-		
-		
+        TreeNode *curNode = root, *preNode;
+        while (curNode != nullptr) {
+            if (curNode->left == nullptr) {
+                curNode = curNode->right;                
+            } else {
+                preNode = curNode->left;
+                while (preNode->right != nullptr)  preNode = preNode->right;
+                
+                preNode->right = curNode->right;
+                curNode->right = curNode->left;
+                curNode->left = nullptr;
+                curNode = curNode->right;   
+            } 
+        }
     }
 };
 

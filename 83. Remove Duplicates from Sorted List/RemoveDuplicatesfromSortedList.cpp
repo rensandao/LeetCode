d9351b7df2abdc0dc12aca8150e8ba0d493d99c3
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-27 00:25:32 +0800

    Update RemoveDuplicatesfromSortedList.cpp

--- 83. Remove Duplicates from Sorted List/RemoveDuplicatesfromSortedList.cpp
+++ 83. Remove Duplicates from Sorted List/RemoveDuplicatesfromSortedList.cpp
@@ -14,16 +14,16 @@ First Creation: 2018-12-26
 
  /**********************************************************1***************************************************************************
  181226
- 思路：
+ 思路1：迭代。
  循环条件是根据当前结点是否为空指针。
  1.如果下一结点p指向的值等于当前结点指向的值，则当前结点指向下下个结点，下个结点指向为空（注意顺序不能反）。
  2.如果前后结点的值不等，则当前结点更新为下一结点。
  
  Runtime: 12ms, 42.64% faster.
  ***********************************************************1**************************************************************************/
-class Solution {
+class Solution1 {
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
+    ListNode *deleteDuplicates(ListNode *head) {
         ListNode *cur = head;
         
         while(cur != NULL) {
@@ -40,3 +40,22 @@ public:
         return head;
     }
 };
+
+
+/*
+181227
+思路2：递归。
+
+*/
+class Solution2 {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+
+    }
+};
+
+
+
+
+
+

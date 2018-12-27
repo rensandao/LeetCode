f42ca6b5bba1fe1d62e229f94b73dbd028901dd3
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-27 23:04:51 +0800

    Update RemoveDuplicatesfromSortedList.cpp

--- 083. Remove Duplicates from Sorted List/RemoveDuplicatesfromSortedList.cpp
+++ 083. Remove Duplicates from Sorted List/RemoveDuplicatesfromSortedList.cpp
@@ -2,6 +2,17 @@
 Source:
 Author: rensandao
 First Creation: 2018-12-26
+
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example 1:
+
+Input: 1->1->2
+Output: 1->2
+Example 2:
+
+Input: 1->1->2->3->3
+Output: 1->2->3
 *****************************************/
 /**
  * Definition for singly-linked list.

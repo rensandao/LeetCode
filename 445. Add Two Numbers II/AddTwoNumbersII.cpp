85847f05aafe483f32ad4d71666a8c3bf6099a6a
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-28 00:12:56 +0800

    Update AddTwoNumbersII.cpp

--- 445. Add Two Numbers II/AddTwoNumbersII.cpp
+++ 445. Add Two Numbers II/AddTwoNumbersII.cpp
@@ -12,7 +12,20 @@ First creation：2018-12-27
  * };
  */
  
- 
+/*
+Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 8 -> 0 -> 7
+
+181228
+思路：
+1）确定两个链表的长度。
+2）进位怎么解决？；
+
+
+
+
+
+*/
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {

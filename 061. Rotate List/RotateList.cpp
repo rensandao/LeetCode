e292bc60f386229808ca08567b47cec679f9cbc0
rensandao <38071371+rensandao@users.noreply.github.com>
2018-12-28 23:35:07 +0800

    Create RotateList.cpp

---
+++ 061. Rotate List/RotateList.cpp
@@ -0,0 +1,19 @@
+/*************************************
+Source:
+Author: rensandao
+First Creation: 2018-12-29
+**************************************/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+    }
+};

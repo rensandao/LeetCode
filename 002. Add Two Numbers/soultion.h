a18362bbb74bf95690b534f1fe7d6589ab1b6422
rensandao <38071371+rensandao@users.noreply.github.com>
2019-01-01 23:40:00 +0800

    Create soultion.h

---
+++ 002. Add Two Numbers/soultion.h
@@ -0,0 +1,29 @@
+/************************
+Source:
+Author: rensandao
+First created： 2018-12-27
+*************************/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+/*
+181228
+思路1： 递归。
+可挖掘的信息有： 1）需要考虑到有进位和两链表不同长短。
+
+Reference:
+
+*/
+//
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+    }
+}]

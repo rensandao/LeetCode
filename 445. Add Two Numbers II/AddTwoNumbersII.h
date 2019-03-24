beaf1874df0e0c319471f98a2d7963c30f910dd9
rensandao <38071371+rensandao@users.noreply.github.com>
2019-03-24 18:27:17 +0800

    Create AddTwoNumbersII.h

---
+++ 445. Add Two Numbers II/AddTwoNumbersII.h
@@ -0,0 +1,20 @@
+#ifndef ADD_TWO_NUMBERSII_H
+#define ADD_TWO_NUMBERSII_H
+
+struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode(int x) : val(x), next(nullptr) {}
+    };
+//declaration for head insert method 
+ListNode* HeadInsert(int sum, ListNode* res);
+
+namespace lcpp {
+class Solution445_1 {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2);
+};
+
+}
+
+#endif // ADD_TWO_NUMBERSII_H

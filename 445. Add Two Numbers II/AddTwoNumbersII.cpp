3bbbcf0860b1e7c769c6e7828813a41caf7d1c52
rensandao <38071371+rensandao@users.noreply.github.com>
2019-03-25 23:48:50 +0800

    Update AddTwoNumbersII.cpp

--- 445. Add Two Numbers II/AddTwoNumbersII.cpp
+++ 445. Add Two Numbers II/AddTwoNumbersII.cpp
@@ -19,7 +19,7 @@ First Created: 2018-12-28
 
 using namespace lcpp;
 
-/*
+/*190324
 整体思路:
 R1:
 限制条件：不修改原链表的结构，不反转链表。
@@ -36,7 +36,7 @@ R1:
 
 */
 
-//method1
+//method1 
 /*仅利用两链长度为突破口；
 1.利用长度差信息和链表指针移位操作，先从链头到尾的顺序，对应位置数据相加，得到的结果（可能大于10）再采用头插法的方式，
 构建成新的链表。现在的结果是反向顺序（即表头是个位数相加结果，靠近表尾是高位相加结果，同时进位问题没有处理）
@@ -49,29 +49,29 @@ ListNode* Solution445_1::addTwoNumbers(ListNode* l1, ListNode* l2) {
 
 	//get each length for use
 	while (ptr1) {
-			ptr1 = ptr1->next;
-			len1++;
+		ptr1 = ptr1->next;
+		len1++;
 	}
 	while (ptr2) {
-			ptr2 = ptr2->next;
-			len2++;
+		ptr2 = ptr2->next;
+		len2++;
 	}
 
 	//step1: strategy to operate addition
 	ptr1 = l1; ptr2 = l2;
 	while (len1 > 0 && len2 > 0) {
-			int sum = 0;
-			if (len1 >= len2) {
-					sum += ptr1->val;
-					ptr1 = ptr1->next;
-					len1--;
-			}
-			if (len1 < len2) {
-					sum += ptr2->val;
-					ptr2 = ptr2->next;
-					len2--;
-			}
-			res = HeadInsert(sum, res);
+		int sum = 0;
+		if (len1 >= len2) {
+				sum += ptr1->val;
+				ptr1 = ptr1->next;
+				len1--;
+		}
+		if (len1 < len2) {
+				sum += ptr2->val;
+				ptr2 = ptr2->next;
+				len2--;
+		}
+		res = HeadInsert(sum, res);
 	}
 	
 	//step2: carry and inverse

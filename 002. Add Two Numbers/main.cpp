ec955138d3ee1e0d3cf639208f8777a8320bb406
rensandao <38071371+rensandao@users.noreply.github.com>
2019-03-26 18:16:48 +0800

    Update main.cpp

--- 002. Add Two Numbers/main.cpp
+++ 002. Add Two Numbers/main.cpp
@@ -4,11 +4,11 @@
 using namespace lcpp;
 
 //reference
-ListNode *new_linkedlist(std::initializer_list<int> lst)
+ListNode *new_linkedlist(std::initializer_list<int> list)
 {
-    auto iter = lst.begin();
-    ListNode *head = lst.size() ? new ListNode(*iter++) : NULL;
-    for (ListNode *cur=head; iter != lst.end(); cur=cur->next)
+    auto iter = list.begin();
+    ListNode *head = list.size() ? new ListNode(*iter++) : nullptr;
+    for (ListNode *cur=head; iter != list.end(); cur=cur->next)
         cur->next = new ListNode(*iter++);
     return head;
 }
@@ -18,9 +18,9 @@ int main()
     Solution002_1 s;
     ListNode *l1 = new_linkedlist({2,4,3});
     ListNode *l2 = new_linkedlist({5,6,4});
-    ListNode *ret = s.addTwoNumbers(l1, l2);
+    ListNode *res = s.addTwoNumbers(l1, l2);
     
-    for (ListNode *cur = ret; cur; cur = cur->next)
+    for (ListNode *cur = res; cur; cur = cur->next)
         std::cout << cur->val << "->";
     std::cout << "\b\b  " << std::endl;
 
